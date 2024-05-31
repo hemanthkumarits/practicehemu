5381976822070f1b12f02b4d3790df93b5ac6239
hemanthkumarits <147583855+hemanthkumarits@users.noreply.github.com>
2024-05-31 15:24:54 +0530

    Add files via upload

---
+++ bootstarp.spec.js
@@ -0,0 +1,37 @@
+const{test,expect}=require('@playwright/test');
+test('bootstarp',async ({page})=>{
+    await page.goto('https://jquery-az.com/boots/demo.php?ex=63.0_2');
+    await page.locator('.multiselect').click();
+    //1
+    //const options = await page.locator('ul>li label input')
+    //await expect(options).toHaveCount(11);
+
+    //2
+    //const options = await page.$$('ul>li label input')
+    //await expect(options.length).toBe(11);
+
+    //select multiple options
+    /*const options = await page.$$('ul>li label')
+    for(let option of options){
+        const value = await option.textContent();
+        //console.log('value:',value);
+        if(value.includes('Angular') || value.includes('Java')){
+            await option.click();
+        }
+
+    }*/
+
+    //deselct
+    const options = await page.$$('ul>li label')
+    for(let option of options){
+        const value = await option.textContent();
+        //console.log('value:',value);
+        if(value.includes('HTML') || value.includes('CSS')){
+            await option.click();
+        }
+
+    }
+
+
+    await page.waitForTimeout(5000)
+})
